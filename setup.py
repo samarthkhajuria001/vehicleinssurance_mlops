1cee4781adefb4399c64818f7719d7c31d42cab6
Samarth Khajuria <kingfisher@Samarths-MacBook-Pro.local>
2025-11-24 06:23:30 +0530

    chore: configure project packaging and install

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Samarth Khajuria",
+    author_email="samkhajuria0@gmail.com",
+    packages=find_packages()
+)
